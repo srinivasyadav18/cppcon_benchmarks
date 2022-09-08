8882fe26348a7ad5cf531dd76bc07e98023e3104
srinivasyadav18 <srinivasyadav227@icloud.com>
2022-09-08 12:26:20 -0500

    benchmarks resulst for for_each_mem_sr_full on skylake

---
+++ benchmarks_results/intel_skylake/for_each_mem_sr_full/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /work/srinivasyadav227/cppcon/cppcon_benchmarks/benchmarks/for_each_mem_sr_full
+# Build directory: /work/srinivasyadav227/cppcon/cppcon_benchmarks/build_medusa03/benchmarks/benchmarks/for_each_mem_sr_full
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(for_each_mem_sr_full_test "/work/srinivasyadav227/cppcon/cppcon_benchmarks/build_medusa03/benchmarks/benchmarks/for_each_mem_sr_full/for_each_mem_sr_full" "--end=30")
+set_tests_properties(for_each_mem_sr_full_test PROPERTIES  _BACKTRACE_TRIPLES "/work/srinivasyadav227/cppcon/cppcon_benchmarks/CMakeLists.txt;26;add_test;/work/srinivasyadav227/cppcon/cppcon_benchmarks/benchmarks/for_each_mem_sr_full/CMakeLists.txt;1;add_simd_test;/work/srinivasyadav227/cppcon/cppcon_benchmarks/benchmarks/for_each_mem_sr_full/CMakeLists.txt;0;")
